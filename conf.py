c97159110090f59453e4fe8110bc0fff24025d96
rockelf <61728142+rockelf@users.noreply.github.com>
2020-03-03 16:54:10 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": true,
+    "repo": "rockelf/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
